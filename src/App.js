ef8ee89af3333b9f5de38efb3951602680ff84fb
Pritesh <pritesh@PriteshJ-0169.local>
2019-03-12 11:19:42 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            React is fun!
           </a>
         </header>
       </div>
